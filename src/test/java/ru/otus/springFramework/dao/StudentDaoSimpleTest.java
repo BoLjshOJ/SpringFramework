9271a79fc0043b81037f8ba5b89d568f8d36469e
BoLjshOJ <boljshoj@gmail.com>
2019-03-25 12:20:04 +0300

    HW3 -update test for StudentDaoSimple

--- src/test/java/ru/otus/springFramework/dao/StudentDaoSimpleTest.java
+++ src/test/java/ru/otus/springFramework/dao/StudentDaoSimpleTest.java
@@ -2,20 +2,31 @@ package ru.otus.springFramework.dao;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.otus.springFramework.domain.Student;
+import ru.otus.springFramework.service.IOService;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StudentDaoSimpleTest {
 
+    @Autowired
+    private StudentDao studentDao;
+    @MockBean
+    private IOService ioService;
+
     @Test
     public void createStudent() {
-        Student student = new Student("name", "surname");
-        assertThat(student.getName()).isEqualToIgnoringCase("name");
-        assertThat(student.getSurname()).isEqualToIgnoringCase("surname");
+        Student studentExpected = new Student("name", "surname");
+        given(ioService.getStringFromSonsole()).willReturn(studentExpected.getName(), studentExpected.getSurname());
+        Student studentActual = studentDao.createStudent();
+        assertThat(studentActual.getName()).isEqualTo(studentExpected.getName());
+        assertThat(studentActual.getSurname()).isEqualTo(studentExpected.getSurname());
     }
 }
